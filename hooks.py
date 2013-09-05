2e0a34de6be317b7b68dee55c3a14ef8d7f6a9a3
Todd Wolfson <todd@twolfson.com>
2013-09-04 23:35:45 -0700

    Wrote out all hooks

--- hooks.py
+++ hooks.py
@@ -1,1 +1,26 @@
 import sublime_plugin
+
+class HooksListener(sublime_plugin.EventListener):
+    def on_new(self, view):
+        pass
+
+    def on_clone(self, view):
+        pass
+
+    def on_load(self, view):
+        pass
+
+    def on_close(self, view):
+        pass
+
+    def on_pre_save(self, view):
+        pass
+
+    def on_post_save(self, view):
+        pass
+
+    def on_activated(self, view):
+        pass
+
+    def on_deactivated(self, view):
+        pass
